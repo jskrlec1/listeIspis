6ac040b9e2e78a97c7289e34702c4672e73805d0
jskrlec1 <josip.skrlecvk@gmail.com>
2023-11-19 11:12:15 +0100

    react-bootstrap

--- src/Components/Ispisimena.jsx
+++ src/Components/Ispisimena.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 // Komponenta Ispisimena koja prima listu kao prop
 const Ispisimena = ({ listaImena }) => {
   return (
     <div>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
+
       <h2>Popis imena</h2>
       <ul>
         {/* Ispisivanje elemenata liste */}
